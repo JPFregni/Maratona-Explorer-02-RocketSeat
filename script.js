b237cd1a18cb519ceb7f3206df461f58002d7bc6
João Paulo Stefani Fregni <45856037+JPFregni@users.noreply.github.com>
2022-07-06 20:52:05 -0300

    Criado js e botões interativos
    Criado arquivo js para o projeto e criado botões com interatividade para a mudança de cor do website

---
+++ script.js
@@ -0,0 +1,19 @@
+function changeLB() {
+    let corpo = document.body
+    corpo.setAttribute('class', '') 
+}
+
+function changeDB() {
+    let corpo = document.body
+    corpo.setAttribute('class', 'dark-blue')
+}
+
+function changeLP() {
+    let corpo = document.body
+    corpo.setAttribute('class', 'pink')
+}
+
+function changeDP() {
+    let corpo = document.body
+    corpo.setAttribute('class', 'dark-pink')
+}
