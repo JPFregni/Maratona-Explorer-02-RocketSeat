1c3f880295d827b6d24eae2218cd012e0dde459e
João Paulo Stefani Fregni <45856037+JPFregni@users.noreply.github.com>
2022-07-10 11:48:51 -0300

    Cores alteram de acordo com hórario
    Cores alteram de acordo com horário, entre o padrão de paleta azul

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ function changeDP() {
     let corpo = document.body
     corpo.setAttribute('class', 'dark-pink')
 }
+
+function carregar() {
+    let corpo = document.body
+    let data = new Date()
+    let agora = data.getHours()
+    if(agora <18){
+        corpo.setAttribute('class', '') 
+    } else {
+        corpo.setAttribute('class', 'dark-blue')
+    }
+}
